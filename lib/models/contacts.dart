c5b01820c4830e99fd67b424ead6e6ecd29e5473
Rosemary91 <rosebel.duah47@gmail.com>
2021-12-17 08:15:35 -0800

    initial commit

---
+++ lib/models/contacts.dart
@@ -0,0 +1,30 @@
+class Contacts{
+  final String contactName;
+  final String contactNumber;
+  final String contactImagepath;
+  final String contactEmail;
+  final String contactLocation;
+
+  Contacts(this.contactName,this.contactNumber,this.contactImagepath,this.contactEmail,this.contactLocation);
+}
+
+List<Contacts> contactlistA = [
+  Contacts("Aonalisa Abane", "+233-72019345", "drawables/ts2.jpg","aonalisaab@gmail.com","Gaza City"),
+  Contacts("Aensah Richard", "+233-72019345", "drawables/ts2.jpg","aesahrich@gmail.com","Kanda Accra"),
+  // Contacts("Perimac Codex", "+233-72019345", "drawables/ts2.jpg"),
+  // Contacts("Frimpong Abane", "+233-72019345", "drawables/ts2.jpg"),
+];
+
+List<Contacts> contactlistB = [
+  Contacts("Bernard Abane", "+233-72019345", "drawables/ts2.jpg","benab@gmail.com","New Jersey"),
+  Contacts("Bismark Richard", "+233-72019345", "drawables/ts2.jpg","bisrich34@yahoo.com","Oakland New Jersey"),
+  Contacts("Belinda Codex", "+233-72019345", "drawables/ts2.jpg","bcodex14@yahoo.com","Conakory City Zambia"),
+  Contacts("Bridget Owusua", "+233-72019345", "drawables/ts2.jpg","owusua34@gmail.com","Paris City"),
+];
+
+List<Contacts>recentList = [
+  Contacts("Mike Stoughton", "+233-547294522", "drawables/ts2.jpg","mikestou89@yahoo.com","South Dacota"),
+  Contacts("Logan Reinhard", "+233-784561238", "drawables/ts2.jpg","lorei214@gmail.com","Indeannapolis United States"),
+  Contacts("Miranda Boonke", "+233-480127345", "drawables/ts2.jpg","mirabooke33@yahoo.com","London City"),
+  Contacts("Rahim Yakubu", "+233-459678234", "drawables/ts2.jpg","rahikubu56@gmail.com","New Hampshire France"),
+];
